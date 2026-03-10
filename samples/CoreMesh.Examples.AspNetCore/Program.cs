23f5c853a54358835f912e687823edcbc6b6f922
DavidChen617 <a880617c@gmail.com>
2026-03-10 19:32:41 +0800

    build: register Result project in solution and samples
    - Add src/Result and tests/CoreMesh.Result.Tests to CoreMesh.slnx
    - Reference CoreMesh.Result in sample project

--- samples/CoreMesh.Examples.AspNetCore/Program.cs
+++ samples/CoreMesh.Examples.AspNetCore/Program.cs
@@ -3,11 +3,14 @@ using CoreMesh.Dispatching.Extensions;
 using CoreMesh.Endpoints.Extensions;
 using CoreMesh.Http.Extensions;
 using CoreMesh.Mapper.Extensions;
+using CoreMesh.Result;
+using CoreMesh.Result.Extensions;
+using CoreMesh.Result.Http;
 using CoreMesh.Validation.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
-    .AddEndpoints()
+    .AddEndpoints([typeof(Program).Assembly])
     .AddApiVersioning(options =>
     {
         options.DefaultApiVersion = new ApiVersion(1);
@@ -23,7 +26,7 @@ builder.Services.AddOpenApi();
 builder.Services
     .AddDispatching([typeof(Program).Assembly])
     .AddCoreMeshMapper([typeof(Program).Assembly])
-    .AddValidation()
+    .AddValidatable()
     .AddCoreMeshHttp();
 
 // builder.Services.AddLogging(logging => { });
@@ -35,8 +38,20 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.MapPost("test", (Test1 t) => Result<Test1>.Ok(new Test1 { Test = t.Test }).ToHttpResult());
+
 app.UseHttpsRedirection();
 app.UseCoreMeshHttp();
 app.MapEndpoints();
 
 app.Run();
+
+
+class Test1
+{
+    public sbyte Test { get; set; }
+}
+class Test2
+{
+    public sbyte Test { get; set; }
+}
