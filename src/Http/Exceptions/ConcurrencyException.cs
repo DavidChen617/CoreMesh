7d915da0705b9bb64964229ec5af9293d280498e
DavidChen617 <a880617c@gmail.com>
2026-03-10 23:03:01 +0800

    chore(http): remove CoreMesh.Http package and its tests

--- src/Http/Exceptions/ConcurrencyException.cs
+++
@@ -1,8 +0,0 @@
-namespace CoreMesh.Http.Exceptions;
-
-/// <summary>
-/// Represents a concurrency conflict exception.
-/// </summary>
-/// <param name="message">The exception message.</param>
-public sealed class ConcurrencyException(string message)
-    : ConflictException(message);
